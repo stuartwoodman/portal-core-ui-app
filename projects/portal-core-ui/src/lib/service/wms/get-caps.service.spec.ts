c417fa84af8ac1f338ace3c40ce26d9539b6f756
Neda Taherifar <n.taherifar@gmail.com>
2022-03-16 12:39:50 +0800

    Small changes to get rid of test error

--- projects/portal-core-ui/src/lib/service/wms/get-caps.service.spec.ts
+++ projects/portal-core-ui/src/lib/service/wms/get-caps.service.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GetCapsService } from './get-caps.service';
 
 describe('GetCapsService', () => {
-  let service: GetCapsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GetCapsService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [GetCapsService]
+  }));
 
   it('should be created', () => {
+    const service: GetCapsService = TestBed.inject(GetCapsService);
     expect(service).toBeTruthy();
-  });
+   });
 });
