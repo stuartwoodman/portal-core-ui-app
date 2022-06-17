0641cfff21092b9a2f55fbee7fcf429cd5cb02b2
Vincent Fazio <vjf@users.noreply.github.com>
2022-06-17 14:49:10 +1000

    AUS-3655 Add "AND" of terms capability to cql_filter

--- projects/portal-core-ui/src/lib/service/cql/cql.service.ts
+++ projects/portal-core-ui/src/lib/service/cql/cql.service.ts
@@ -1,5 +1,3 @@
-
-
 export class CQLService {
 
     /**
@@ -14,15 +12,26 @@ export class CQLService {
             if (filt.predicate.toUpperCase() === 'CQL_LIKE') {
                 // Transform the selected filter values into a CQL query string
                 let sub_str = "";
-                // Array of values
+                // If there is an array of values
                 if (filt.value instanceof Array) {
+                    // Construct a string with terms separated by "OR" or "AND"
                     for (const val of filt.value) {
-                        sub_str += filt.xpath + " LIKE '%" + val + "%' OR "
+                        sub_str += filt.xpath + " LIKE '%" + val + "%' ";
+                        // "OR" is default
+                        if (filt.hasOwnProperty('boolOp')) {
+                            sub_str += filt.boolOp + " ";
+                        } else {
+                            sub_str += "OR ";
+                        }
                     }
-                    // Trim off " OR " at end of string
-                    sub_str = sub_str.substring(0, sub_str.length - 4);
+                    // Trim off " AND " or " OR "  at end of string
+                    if (filt.hasOwnProperty('boolOp')) {
+                        sub_str = sub_str.substring(0, sub_str.length - (filt.boolOp.length + 2));
+                    } else {
+                        sub_str = sub_str.substring(0, sub_str.length - 4);
+                    }
 
-                // Single value
+                // If there is a single value
                 } else if (typeof filt.value === "string") {
                     sub_str = filt.xpath + " LIKE '%" + filt.value + "%'";
                 }
